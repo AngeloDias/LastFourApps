ba6ba461ba00546eb72562b2426492b42aa404e8
angelodias <angelossdias@gmail.com>
2020-05-05 17:42:44 -0300

    Adding anonymous Firebase authentication

--- FindMyPhoneApp/app/src/main/java/br/com/training/android/findmyphoneapp/LoginActivity.kt
+++ FindMyPhoneApp/app/src/main/java/br/com/training/android/findmyphoneapp/LoginActivity.kt
@@ -1,15 +1,21 @@
 package br.com.training.android.findmyphoneapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        signInAnonymously()
 
         btnRegister.setOnClickListener {
             val userData = UserData(applicationContext)
@@ -19,6 +25,21 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    private fun signInAnonymously() {
+        mAuth!!.signInAnonymously().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(baseContext, "Authentication successful.", Toast.LENGTH_SHORT).show()
+                val user = mAuth!!.currentUser
+
+            } else {
+                // If sign in fails, display a message to the user.
+                Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
 }
