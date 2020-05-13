de41200c003621a6b6df18c3be30bfe3484c0a17
angelodias <angelossdias@gmail.com>
2020-05-13 16:12:54 -0300

    Initializing twitter app

---
+++ MyTwitterApp/app/src/main/java/br/com/training/android/mytwitterapp/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package br.com.training.android.mytwitterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btnRegister.setOnClickListener {}
+    }
+}
